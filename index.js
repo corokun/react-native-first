a4c5dd107345f69225d951b9e28eb534ddd83de4
frederickalcantara <frederickaalcantara@gmail.com>
2019-02-20 00:29:12 -0500

    Installed React Native Navigation & got Android and IOS to render on their respective emulators

--- index.js
+++ index.js
@@ -4,10 +4,9 @@
  */
 
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
 import { Provider } from 'react-redux';
 import App from './App';
-import {name as appName} from './app.json';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -18,4 +17,17 @@ const RNRedux = () => (
   </Provider>
 )
 
-AppRegistry.registerComponent(appName, () => RNRedux);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => RNRedux);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
+
+
+
