1d47b32152df2f7d1bf0da1c0c302edeadb8fe96
frederickalcantara <frederickaalcantara@gmail.com>
2019-02-20 23:23:54 -0500

    Adding a top bar to get title rendered on ios & Android, currently trying to get ios on the screen

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import React from 'react';
 import { Navigation } from "react-native-navigation";
 import { Provider } from 'react-redux';
 import App from './App';
+
+import AuthScreen from './src/screens/Auth';
+
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -17,17 +20,25 @@ const RNRedux = () => (
   </Provider>
 )
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => RNRedux);
+Navigation.registerComponent('AuthScreen', () => AuthScreen);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component: {
-        name: "navigation.playground.WelcomeScreen"
+      stack: {
+        children: [{
+          component: {
+            name: 'AuthScreen'
+          }
+        }],
+        options: {
+          topBar: {
+            title: {
+              text: 'Login'
+            }
+          }
+        }
       }
     }
   });
 });
-
-
-
