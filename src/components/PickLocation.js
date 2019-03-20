87cf565f76bf7317a318416c255ee76361347301
frederickalcantara <frederickaalcantara@gmail.com>
2019-03-20 18:55:02 -0400

    Picked location on the map

--- src/components/PickLocation.js
+++ src/components/PickLocation.js
@@ -18,12 +18,25 @@ class PickLocation extends Component {
     super(props);
   }
 
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.setState(prevState => ({
+      focusedLocation: {
+        ...prevState.focusedLocation,
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      },
+    }));
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
           initialRegion={this.state.focusedLocation}
+          region={this.state.focusedLocation}
           style={styles.map}
+          onPress={this.pickLocationHandler}
         />
         <View style={styles.button}>
           <Button
