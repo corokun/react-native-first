a519bbf68023fcf6c85c890231cc8c0ae1ea8927
frederickalcantara <frederickaalcantara@gmail.com>
2019-03-22 21:28:52 -0400

    Added redux thunk and the ability to add places to firebase api

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
-export const addPlace = (placeName, location, image) => ({
-  type: ADD_PLACE,
-  placeName,
-  location,
-  image,
-});
+export const addPlace = (placeName, location, image) => dispatch => {
+  const placeData = {
+    name: placeName,
+    location,
+  };
+  fetch('https://rn-course-1552961879445.firebaseio.com/places.json', {
+    method: 'POST',
+    body: JSON.stringify(placeData),
+  })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
+    });
+};
 
 export const deletePlace = key => ({
   type: DELETE_PLACE,
